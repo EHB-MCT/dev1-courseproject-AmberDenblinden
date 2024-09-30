c97c8061f05d1a6fae22c36c8388b6a09aee9f28
Amber Denblinden <amber.denblinden@student.ehb.be>
2024-09-30 16:27:54 +0200

    recreated-avatar

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,25 @@
 "use strict";
 
-alert("Draw your space invader here");
+
+signature();
+
+function signature() {
+    
+    let canvas = document.querySelector("canvas");
+   canvas.width = window.innerWidth;
+   canvas.height = window.innerHeight;
+   let context = canvas.getContext("2d");
+
+   //big rectangle
+   context.fillStyle= "lightgray"
+   context.fillRect(50,50,300,300);
+
+   //small rectangles
+   context.fillStyle="lightgreen"
+   context.fillRect(125,75,50,50);
+   context.fillRect(225,75,50,50);
+   context.fillRect(175,125,50,100);
+   context.fillRect(75,175,50,50);
+   context.fillRect(275,175,50,50);
+   context.fillRect(125,225,150,100);
+}
